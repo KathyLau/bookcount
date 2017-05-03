2f97fe5544ab06a6446c3d978dda088d3bac232e
Kathy Lau <kathy@mit.edu>
2017-05-02 21:34:26 -0400

    first two

---
+++ map_reduce.py
@@ -0,0 +1,12 @@
+f = open("book.txt","r")
+story = f.read()
+
+book = [word.strip('(",.)!?-') for word in story.split()]
+
+def freq(word1):
+    return len(filter(lambda word2: word2 == word1, book))
+
+def freqGroup(group):
+    return str(reduce(lambda a,b: a + b, [freq(word) for wood in group]))
+
+print freq("Chapter")
